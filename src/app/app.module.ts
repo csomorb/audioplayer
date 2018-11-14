0fcb989b4047563b270fffc2e747f196de12ecb5
barnabas.csomor@alliance-reseaux.net <barnabas.csomor@alliance-reseaux.net>
2018-11-14 16:26:33 +0100

    audioplayer init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AudioProvider } from '../providers/audio/audio';
+import { MusiclistProvider } from '../providers/musiclist/musiclist';
+import { StoreModule } from '@ngrx/store';
+import { mediaStateReducer } from '../providers/store/store';
 
 @NgModule({
   declarations: [
@@ -14,6 +21,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
+    StoreModule.forRoot({appState : mediaStateReducer }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +35,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AudioProvider,
+    MusiclistProvider,
   ]
 })
 export class AppModule {}
